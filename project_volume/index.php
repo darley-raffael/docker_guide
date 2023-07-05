d91e26dc746b12c4777a3176222c00ffbdde0784
Dárley Raffael <darleyraffael@gmail.com>
2023-07-05 19:39:50 -0300

    fix: atualiza integração com servidor

---
+++ project_volume/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Formulário em php</title>
+</head>
+<body>
+  <h1>Digite os dados aqui</h1>
+  <form action="process.php" method="POST">
+    <input type="text" name="message"id="message" >
+    <input type="submit" value="Enviar Mensagem">
+  </form>
+</body>
+</html>
